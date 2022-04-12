c4e38d2027775c9f326c70f6602834baeeafeebc
Juubaju <julia.pereira133@etec.sp.gov.br>
2022-04-12 12:33:54 -0300

    first commit

---
+++ Resultado.class.php
@@ -0,0 +1,17 @@
+    <?php
+    require_once("2.php");
+
+    class Resultado{
+        private $nome;
+        private $email;
+
+       public function _construct(){
+           $cadastro = new Cadastro();
+           $cadastro->setNome($_POST["nome"]);
+           $cadastro->setEmail($_POST["email"]);
+
+           echo $cadastro->getNome() . "br" . $cadastro->getEmail();
+
+           }
+    }
+    ?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Document</title>
+</head>
+<body>
+	<form action="Resultado.class.php" method="POST">
+		<label for="lblNome">Nome</label>
+		<input type="text" name="txtNome"><br>
+        <label for="iblemail">Email</label>
+        <input type="text" nome = "txtEmail">
+        <button>Cadastrar</button>
+
+	</form>
+</body>
+</html>
